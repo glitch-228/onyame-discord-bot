b066437341dee1b5b5456bc9f4b13987869423d5
glitch228 <66838958+DJPLAY420@users.noreply.github.com>
2022-04-30 00:03:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'OTY5NjUwODg1NTMxNjc2NzEy.Ymwfwg.r38iCx6_FYnpYFXWkwgHOFSfk_w',
-    'bot': 'anime glitch bot',
-    'id': 969650885531676712,
-    'prefix': 'ga!'
+    'token': 'token',
+    'bot': 'name',
+    'id': client_id,
+    'prefix': 'prefix'
 }
